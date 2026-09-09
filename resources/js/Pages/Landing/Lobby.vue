c433bfa6dd02208cc950158349de3f5ecd1333ff
SerchPer11 <paso220489@upemor.edu.mx>
2026-09-09 14:43:15 -0600

    Feat(lobby): Implement full lobby views

---
+++ resources/js/Pages/Landing/Lobby.vue
@@ -0,0 +1,13 @@
+<script setup>
+import MainLayout from '@/Layouts/Lobby/MainLayout.vue';
+import Hero from '@/Pages/Landing/Components/Hero.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+  <Head title="Streetwear para Bulldogs y sus Humanos" />
+
+  <MainLayout>
+    <Hero />
+  </MainLayout>
+</template>
